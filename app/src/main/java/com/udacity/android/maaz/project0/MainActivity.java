69cea8301ca646998c3579a57de0966b022a8e54
Maaz Syed Adeeb <maaz.adeeb@sap.com>
2015-11-22 10:41:56 +0530

    Creating generic button style in order to reduce XML code in content_main.xml. Also, incorporating changes as suggested by the Udacity Reviewer.

--- app/src/main/java/com/udacity/android/maaz/project0/MainActivity.java
+++ app/src/main/java/com/udacity/android/maaz/project0/MainActivity.java
@@ -6,16 +6,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private static String SPOTIFY = "Spotify Streamer";
-    private static String SUPER_DUO = "Super Duo";
-    private static String BUILD_BIGGER = "Build It Bigger";
-    private static String XYZ = "XYZ Reader";
-    private static String CAPSTONE = "Capstone";
-    private static String UNKNOWN = "Unknown";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,36 +45,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        String appName = null;
+        // Get the button clicked and its corresponding text
+        Button button = (Button) findViewById(v.getId());
+        String appName = button.getText().toString();
+        String toastString = "This button will launch my " + appName + "!";
 
-        switch (v.getId()) {
-
-            case R.id.spotifyButton:
-                appName = SPOTIFY;
-                break;
-
-            case R.id.biggerButton:
-                appName = BUILD_BIGGER;
-                break;
-
-            case R.id.xyzButton:
-                appName = XYZ;
-                break;
-
-            case R.id.scoresButton:
-            case R.id.libraryButton:
-                appName = SUPER_DUO;
-                break;
-
-            case R.id.capstoneButton:
-                appName = CAPSTONE;
-                break;
-
-            default:
-                appName = UNKNOWN;
-        }
-
-        String toastString = "This button will launch my " + appName + " app!";
+        // Create the toast
         Toast.makeText(MainActivity.this, toastString, Toast.LENGTH_SHORT).show();
     }
 }
